074e52c36d70439a2eee4898add3dfe00bd8de31
kikegoni <egecheverria_1993@hotmail.com>
2024-07-16 10:09:44 +0200

    mm39 typo fixed

--- create_indexes.sh
+++ create_indexes.sh
@@ -27,25 +27,25 @@ kb ref -i kallisto_nuclei_introns_index.idx -g kallisto_nuclei_introns_t2g.txt -
 cat kallisto_nuclei_introns_cdna.fa kallisto_nuclei_introns_intron.fa > kallisto_nuclei_cDNA_introns_ALL.fa
 kallisto index -i kallisto_nuclei_cDNA_introns.idx kallisto_nuclei_cDNA_introns_ALL.fa
 
-# Mm10
-mm10_gencode_genome_fasta="/home/egonie/dato-activo/reference.genomes_kike/GRCm39/gencode/GRCm39.primary_assembly.genome.fa"
-mm10_gencode_gtf="/home/egonie/dato-activo/reference.genomes_kike/GRCm39/gencode/gencode.vM27.annotation.gtf"
-mm10_gencode_transcriptome_fasta="/home/egonie/dato-activo/reference.genomes_kike/GRCm39/gencode/gencode.vM27.transcripts.fa"
+# mm39
+mm39_gencode_genome_fasta="/home/egonie/dato-activo/reference.genomes_kike/GRCm39/gencode/GRCm39.primary_assembly.genome.fa"
+mm39_gencode_gtf="/home/egonie/dato-activo/reference.genomes_kike/GRCm39/gencode/gencode.vM27.annotation.gtf"
+mm39_gencode_transcriptome_fasta="/home/egonie/dato-activo/reference.genomes_kike/GRCm39/gencode/gencode.vM27.transcripts.fa"
 
 cd /home/egonie/dato-activo/reference.genomes_kike/GRCm39/gencode
 # Cell Ranger
-cellranger mkref --genome=CELLRANGER_REF --fasta=$mm10_gencode_genome_fasta  --genes=$mm10_gencode_gtf
+cellranger mkref --genome=CELLRANGER_REF --fasta=$mm39_gencode_genome_fasta  --genes=$mm39_gencode_gtf
 
 #STARsolo
-STAR  --runMode genomeGenerate  --genomeDir STAR_index  --genomeFastaFiles $mm10_gencode_genome_fasta --sjdbGTFfile $mm10_gencode_gtf   --sjdbOverhang 100
+STAR  --runMode genomeGenerate  --genomeDir STAR_index  --genomeFastaFiles $mm39_gencode_genome_fasta --sjdbGTFfile $mm39_gencode_gtf   --sjdbOverhang 100
 
 # Kallisto
-kallisto index -i transcriptome_index_kallisto.idx $mm10_gencode_transcriptome_fasta
+kallisto index -i transcriptome_index_kallisto.idx $mm39_gencode_transcriptome_fasta
 
 # Salmon (selective alignment, following https://combine-lab.github.io/alevin-tutorial/2019/selective-alignment/)
-cat $mm10_gencode_genome_fasta | grep ">" |  cut -d " " -f 1 > decoys.txt
+cat $mm39_gencode_genome_fasta | grep ">" |  cut -d " " -f 1 > decoys.txt
 sed -i.bak -e 's/>//g' decoys.txt   # prepare decoys
-cat $mm10_gencode_transcriptome_fasta $mm10_gencode_genome_fasta > gentrome.fa
+cat $mm39_gencode_transcriptome_fasta $mm39_gencode_genome_fasta > gentrome.fa
 salmon index -t gentrome.fa -d decoys.txt -p 12 -i transcriptome_selective_alignment_index_salmon --gencode
 
 
